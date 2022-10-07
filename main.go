1ffc0af88919b7da6bc909e67ebed034cab83d66
NullBy1e <68380685+NullBy1e@users.noreply.github.com>
2022-10-07 21:55:15 +0200

    Small progress

--- main.go
+++ main.go
@@ -1,7 +1,9 @@
 package main
 
 import (
+	"fmt"
 	"log"
+	"net/http"
 	"os"
 	"strconv"
 
@@ -31,15 +33,18 @@ func main() {
 				Usage:       "JSON returned on POST",
 				Destination: &jsonData,
 			},
+			//TODO: Create Output file to JSON
 		},
 		Action: func(ctx *cli.Context) error {
-			port, _ := strconv.Atoi(ctx.Args().Get(1))
-			if port > 99999 {
+
+			port := ctx.Args().Get(0)
+			if num, _ := strconv.Atoi(port); num > 99999 {
 				cli.Exit("Port must be in range 0-99999", 2)
 			}
 
-			if port == 0 {
-				port = 5000
+			log.Println(port)
+			if port == "" {
+				port = "5000"
 			}
 
 			startAPIServer(port, jsonData, fileName)
@@ -52,6 +57,34 @@ func main() {
 
 }
 
-func startAPIServer(port int, postData string, fileName string) {
-	color.HiYellow("Starting the API endpoint on port: %d", port)
+func startAPIServer(port string, postData string, fileName string) {
+	color.HiYellow("Starting the API endpoint on port: " + port)
+
+	if fileName != "" && postData != "" {
+		color.Red("Cannot use --file and --json at the same time.")
+		return
+	} else if fileName != "" {
+		color.Green("Using file data as POST")
+		// TODO: Read the file and send it to server
+	} else if postData != "" {
+		color.Green("Using JSON argument as POST")
+		http.HandleFunc("/", func(w http.ResponseWriter, r *http.Request) {
+			//* Returns the JSON and prints the data about it
+			fmt.Fprintf(w, postData)
+			requestDebugger(w, r)
+		})
+		log.Printf(":" + port)
+		http.ListenAndServe(":"+port, nil)
+	} else {
+		color.Green("Starting basic API server")
+
+		// TODO: Don't bind only to /, rather to everything!
+		http.HandleFunc("/", requestDebugger)
+		http.ListenAndServe(":"+port, nil)
+	}
+}
+
+func requestDebugger(w http.ResponseWriter, r *http.Request) {
+	//* Prints the details of the request
+	log.Println("Incoming Request on:", r.URL)
 }
